2e2e65cc82366ba1f7e6cc9d5cf6365c4ebda15b
jozsef-szilagyi <jozsefszilagyi0@gmail.com>
2026-09-17 12:41:05 +0200

    Registerpage is ready to use

--- UserAdmin/Views/RegisterPage.xaml.cs
+++ UserAdmin/Views/RegisterPage.xaml.cs
@@ -10,6 +10,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using UserAdmin.Models;
+using UserAdmin.Services;
 
 namespace UserAdmin.Views
 {
@@ -18,19 +20,56 @@ namespace UserAdmin.Views
     /// </summary>
     public partial class RegisterPage : Page
     {
+        private readonly UserDbService _userDbService= new UserDbService();
         public RegisterPage()
         {
             InitializeComponent();
-        }
-
-        private void Register_Click(object sender, RoutedEventArgs e)
-        {
-
         }
 
         private void Login_Click(object sender, RoutedEventArgs e)
         {
 
         }
+
+        private void Register_Click(object sender, RoutedEventArgs e)
+        {
+            var username = UsernameBox.Text.Trim();
+            var email = EmailBox.Text.Trim();
+            var password = PasswordBoxInput.Password;
+            var confirmpassword = ConfirmPasswordBox.Password;
+
+            if (string.IsNullOrWhiteSpace(username) || string.IsNullOrWhiteSpace(email) || string.IsNullOrWhiteSpace(password) || string.IsNullOrWhiteSpace(confirmpassword))
+                {
+                ErrorText.Text = "Minden mező kitöltése kötelező";
+                ErrorText.Visibility = Visibility.Visible;
+                return;
+                }
+            if (password.Length < 6)
+            {
+                ErrorText.Text = "A jelszónak legalább 6 karakterből kell állnia.";
+                ErrorText.Visibility = Visibility.Visible;
+                return;
+            }
+            if (password != confirmpassword)
+            {
+                ErrorText.Text = "A két jelszó nem egyezik meg!";
+                ErrorText.Visibility = Visibility.Visible;
+                return;
+            }
+
+            var user = new User
+            {
+                Username = username,
+                Email = email,
+                Password = password,
+                RegisteredAt = DateTime.Now,
+            };
+
+            _userDbService.Add(user);
+
+            MessageBox.Show("Sikeres regisztráció.");
+        }
+
+        
     }
 }
